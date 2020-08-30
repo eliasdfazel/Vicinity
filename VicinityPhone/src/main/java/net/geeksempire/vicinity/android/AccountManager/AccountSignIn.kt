a33dbe495da3134a367e8f9776576f6155a45372
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-08-30 09:56:24 -0800

    [Description] #
    [Phone][ChangeLog]
    2. Gps Checkpoint & Listener & Enabler Added
    1. Network Checkpoint & Listener Added
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ VicinityPhone/src/main/java/net/geeksempire/vicinity/android/AccountManager/AccountSignIn.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2020 By Geeks Empire.
+ *
+ * Created by Elias Fazel on 8/30/20 9:56 AM
+ * Last modified 8/30/20 9:56 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package net.geeksempire.vicinity.android.AccountManager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AccountSignIn : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
