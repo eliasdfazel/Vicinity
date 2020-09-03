804105c6e57331737d627858720ebc2bab7e25c9
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-09-03 08:56:04 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- VicinityPhone/src/main/java/net/geeksempire/vicinity/android/MapConfiguration/Map/MapsOfSociety.kt
+++ VicinityPhone/src/main/java/net/geeksempire/vicinity/android/MapConfiguration/Map/MapsOfSociety.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 9/3/20 8:55 AM
- * Last modified 9/3/20 8:54 AM
+ * Created by Elias Fazel on 9/3/20 8:56 AM
+ * Last modified 9/3/20 8:55 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
