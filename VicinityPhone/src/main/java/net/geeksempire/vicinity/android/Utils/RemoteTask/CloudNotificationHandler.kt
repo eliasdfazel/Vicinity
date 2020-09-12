759ad630432bc28044e6b2da8ab09176ee314c28
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-09-12 06:36:57 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- VicinityPhone/src/main/java/net/geeksempire/vicinity/android/Utils/RemoteTask/CloudNotificationHandler.kt
+++ VicinityPhone/src/main/java/net/geeksempire/vicinity/android/Utils/RemoteTask/CloudNotificationHandler.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 9/12/20 5:17 AM
- * Last modified 9/12/20 5:09 AM
+ * Created by Elias Fazel on 9/12/20 6:36 AM
+ * Last modified 9/12/20 6:36 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -21,8 +21,11 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.google.android.gms.maps.model.LatLng
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import net.geeksempire.vicinity.android.BuildConfig
 import net.geeksempire.vicinity.android.CommunicationConfiguration.Public.Endpoint.PublicCommunicationEndpoint
 import net.geeksempire.vicinity.android.CommunicationConfiguration.Public.PublicCommunityUI.PublicCommunity
 import net.geeksempire.vicinity.android.MapConfiguration.Vicinity.vicinityName
@@ -32,9 +35,11 @@ import net.geeksempire.vicinity.android.Utils.UI.NotifyUser.NotificationCreator
 
 class CloudNotificationHandler : FirebaseMessagingService() {
 
-    val locationCheckpoint = LocationCheckpoint()
+    private val firebaseUser = Firebase.auth.currentUser
 
-    val notificationCreator by lazy {
+    private val locationCheckpoint = LocationCheckpoint()
+
+    private val notificationCreator by lazy {
         NotificationCreator(applicationContext)
     }
 
@@ -43,51 +48,61 @@ class CloudNotificationHandler : FirebaseMessagingService() {
 
         val linkedHashMapData = remoteMessage.data
 
+        val selfUid = linkedHashMapData["selfUid"].toString()
+
         val nameOfCountry = linkedHashMapData["nameOfCountry"].toString()
         val vicinityLocation = LatLng(linkedHashMapData["vicinityLatitude"].toString().toDouble(), linkedHashMapData["vicinityLongitude"].toString().toDouble())
 
-        Glide.with(applicationContext)
-            .asBitmap()
-            .load(linkedHashMapData["notificationLargeIcon"])
-            .diskCacheStrategy(DiskCacheStrategy.ALL)
-            .listener(object : RequestListener<Bitmap> {
-                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
+        firebaseUser?.let {
 
-                    return false
-                }
+            if ((firebaseUser.uid != selfUid) || BuildConfig.DEBUG) {
 
-                override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                Glide.with(applicationContext)
+                    .asBitmap()
+                    .load(linkedHashMapData["notificationLargeIcon"])
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .listener(object : RequestListener<Bitmap> {
+                        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
 
-                    resource?.let { bitmap ->
+                            return false
+                        }
 
-                        val publicCommunityIntent = Intent(linkedHashMapData["publicCommunityAction"].toString())
-                        publicCommunityIntent.putExtra(PublicCommunity.Configurations.PublicCommunityName, linkedHashMapData["publicCommunityName"])
-                        publicCommunityIntent.putExtra(PublicCommunity.Configurations.PublicCommunityDatabasePath, PublicCommunicationEndpoint.publicCommunityDocumentEndpoint(countryName = nameOfCountry, vicinityLocation))
-                        publicCommunityIntent.putExtra(PublicCommunity.Configurations.PublicCommunityCountryName, nameOfCountry)
-                        publicCommunityIntent.putExtra(PublicCommunity.Configurations.PublicCommunityCenterLocationLatitude, vicinityLocation.latitude)
-                        publicCommunityIntent.putExtra(PublicCommunity.Configurations.PublicCommunityCenterLocationLongitude, vicinityLocation.longitude)
-                        publicCommunityIntent.putExtra("fromNotification", true)
+                        override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
 
-                        val publicCommunityPendingIntent: PendingIntent = PendingIntent.getActivity(applicationContext, 666, publicCommunityIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+                            resource?.let { bitmap ->
 
-                        notificationCreator.create(
-                            notificationChannelId = linkedHashMapData["publicCommunityName"].toString(),
-                            titleText = linkedHashMapData["selfDisplayName"].toString(),
-                            contentText = linkedHashMapData["messageContent"].toString(),
-                            largeIcon = bitmap,
-                            notificationColor = extractDominantColor(applicationContext, bitmap),
-                            notificationId = System.currentTimeMillis(),
-                            locationKnownName = locationCheckpoint.knownLocationName(applicationContext, vicinityName(vicinityLocation)).toString(),
-                            pendingIntent = publicCommunityPendingIntent
-                        )
+                                val publicCommunityIntent = Intent(linkedHashMapData["publicCommunityAction"].toString())
+                                publicCommunityIntent.putExtra(PublicCommunity.Configurations.PublicCommunityName, linkedHashMapData["publicCommunityName"])
+                                publicCommunityIntent.putExtra(PublicCommunity.Configurations.PublicCommunityDatabasePath, PublicCommunicationEndpoint.publicCommunityDocumentEndpoint(countryName = nameOfCountry, vicinityLocation))
+                                publicCommunityIntent.putExtra(PublicCommunity.Configurations.PublicCommunityCountryName, nameOfCountry)
+                                publicCommunityIntent.putExtra(PublicCommunity.Configurations.PublicCommunityCenterLocationLatitude, vicinityLocation.latitude)
+                                publicCommunityIntent.putExtra(PublicCommunity.Configurations.PublicCommunityCenterLocationLongitude, vicinityLocation.longitude)
+                                publicCommunityIntent.putExtra("fromNotification", true)
 
-                    }
+                                val publicCommunityPendingIntent: PendingIntent = PendingIntent.getActivity(applicationContext, 666, publicCommunityIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-                    return false
-                }
+                                notificationCreator.create(
+                                    notificationChannelId = linkedHashMapData["publicCommunityName"].toString(),
+                                    titleText = linkedHashMapData["selfDisplayName"].toString(),
+                                    contentText = linkedHashMapData["messageContent"].toString(),
+                                    largeIcon = bitmap,
+                                    notificationColor = extractDominantColor(applicationContext, bitmap),
+                                    notificationId = System.currentTimeMillis(),
+                                    locationKnownName = locationCheckpoint.knownLocationName(applicationContext, vicinityName(vicinityLocation)).toString(),
+                                    pendingIntent = publicCommunityPendingIntent
+                                )
 
-            })
-            .submit()
+                            }
+
+                            return false
+                        }
+
+                    })
+                    .submit()
+
+            }
+
+        }
 
     }
 
