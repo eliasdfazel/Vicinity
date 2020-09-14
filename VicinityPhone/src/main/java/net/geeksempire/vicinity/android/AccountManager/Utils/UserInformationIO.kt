9d96c6bb6e68d086b69d337b644d19775f03303a
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-09-14 08:52:25 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- VicinityPhone/src/main/java/net/geeksempire/vicinity/android/AccountManager/Utils/UserInformationIO.kt
+++ VicinityPhone/src/main/java/net/geeksempire/vicinity/android/AccountManager/Utils/UserInformationIO.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 9/14/20 8:30 AM
- * Last modified 9/14/20 8:03 AM
+ * Created by Elias Fazel on 9/14/20 8:52 AM
+ * Last modified 9/14/20 8:30 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
