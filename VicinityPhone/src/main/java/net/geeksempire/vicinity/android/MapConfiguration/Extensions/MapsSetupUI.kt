a626c25da8b8a261f0865495f69314039e2573a7
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-09-14 06:31:57 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- VicinityPhone/src/main/java/net/geeksempire/vicinity/android/MapConfiguration/Extensions/MapsSetupUI.kt
+++ VicinityPhone/src/main/java/net/geeksempire/vicinity/android/MapConfiguration/Extensions/MapsSetupUI.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 9/13/20 8:01 AM
- * Last modified 9/13/20 8:01 AM
+ * Created by Elias Fazel on 9/14/20 6:31 AM
+ * Last modified 9/14/20 6:30 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -10,9 +10,13 @@
 
 package net.geeksempire.vicinity.android.MapConfiguration.Extensions
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.constraintlayout.widget.ConstraintLayout
 import net.geeksempire.vicinity.android.MapConfiguration.Map.MapsOfSociety
+import net.geeksempire.vicinity.android.Preferences.PreferencesControl
 import net.geeksempire.vicinity.android.Utils.UI.Display.statusBarHeight
+import kotlin.math.roundToInt
 
 fun MapsOfSociety.mapsOfSocietySetupUI() {
 
@@ -20,13 +24,17 @@ fun MapsOfSociety.mapsOfSocietySetupUI() {
     preferenceViewLayoutParams.setMargins(0, preferenceViewLayoutParams.topMargin + statusBarHeight(applicationContext), 0, 0)
     mapsViewBinding.preferenceView.layoutParams = preferenceViewLayoutParams
 
+    setClicksSetup()
+
 }
 
 fun MapsOfSociety.setClicksSetup() {
 
     mapsViewBinding.preferenceView.setOnClickListener {
 
-
+        startActivity(Intent(applicationContext, PreferencesControl::class.java).apply {
+//            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }, ActivityOptions.makeScaleUpAnimation(mapsViewBinding.preferenceView, mapsViewBinding.preferenceView.x.roundToInt(), mapsViewBinding.preferenceView.y.roundToInt(), mapsViewBinding.preferenceView.width, mapsViewBinding.preferenceView.height).toBundle())
 
     }
 
