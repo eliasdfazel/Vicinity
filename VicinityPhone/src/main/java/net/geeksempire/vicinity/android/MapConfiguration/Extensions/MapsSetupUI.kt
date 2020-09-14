7bde831d4ff752380808aa8c8535f30c9c901084
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-09-14 06:43:12 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- VicinityPhone/src/main/java/net/geeksempire/vicinity/android/MapConfiguration/Extensions/MapsSetupUI.kt
+++ VicinityPhone/src/main/java/net/geeksempire/vicinity/android/MapConfiguration/Extensions/MapsSetupUI.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 9/14/20 6:31 AM
- * Last modified 9/14/20 6:30 AM
+ * Created by Elias Fazel on 9/14/20 6:43 AM
+ * Last modified 9/14/20 6:37 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -33,7 +33,7 @@ fun MapsOfSociety.setClicksSetup() {
     mapsViewBinding.preferenceView.setOnClickListener {
 
         startActivity(Intent(applicationContext, PreferencesControl::class.java).apply {
-//            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
         }, ActivityOptions.makeScaleUpAnimation(mapsViewBinding.preferenceView, mapsViewBinding.preferenceView.x.roundToInt(), mapsViewBinding.preferenceView.y.roundToInt(), mapsViewBinding.preferenceView.width, mapsViewBinding.preferenceView.height).toBundle())
 
     }
