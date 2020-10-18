8979695423ba071c21b1892943ca139e798476d7
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-10-18 06:08:30 -0800

    [Description] #
    [Phone][ChangeLog]
    1. Gesture Swipe Added
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- VicinityPhone/src/main/java/net/geeksempire/vicinity/android/CommunicationConfiguration/HistoryConfiguration/HistoryUI/HistoryLists.kt
+++ VicinityPhone/src/main/java/net/geeksempire/vicinity/android/CommunicationConfiguration/HistoryConfiguration/HistoryUI/HistoryLists.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 10/18/20 5:38 AM
- * Last modified 10/18/20 5:38 AM
+ * Created by Elias Fazel on 10/18/20 6:08 AM
+ * Last modified 10/18/20 6:08 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -127,6 +127,20 @@ class HistoryLists : AppCompatActivity(), GestureListenerInterface, NetworkConne
                 historyLiveData.privateHistoryNetworkOperation()
 
             }
+        }
+
+        historyListsViewBinding.publicHistory.setOnClickListener {
+
+            historyLiveData.publicHistoryNetworkOperation()
+
+
+        }
+
+        historyListsViewBinding.privateHistory.setOnClickListener {
+
+            historyLiveData.privateHistoryNetworkOperation()
+
+
         }
 
     }
