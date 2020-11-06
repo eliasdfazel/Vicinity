157ac79927c2527cb31ae8572f3eab696b490dfd
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-11-06 08:23:06 -0900

    [Description] #
    [Phone][ChangeLog]
    1. Invitation Added for Business
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- VicinityPhone/src/main/java/net/geeksempire/vicinity/android/Invitation/Receive/ReceiveInvitationData.kt
+++ VicinityPhone/src/main/java/net/geeksempire/vicinity/android/Invitation/Receive/ReceiveInvitationData.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 11/6/20 7:20 AM
- * Last modified 11/6/20 7:20 AM
+ * Created by Elias Fazel on 11/6/20 8:23 AM
+ * Last modified 11/6/20 8:22 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
